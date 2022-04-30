70c191ea4c904f35f8771d4bc6895dc9b2fd2394
avintechno <avintechno@gmail.com>
2022-04-30 05:24:42 +0000

    Updated HomeController.cs

--- DemoWebApp01/DemoWebApp01/Controllers/HomeController.cs
+++ DemoWebApp01/DemoWebApp01/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace DemoWebApp01.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        
             return View();
         }
 
